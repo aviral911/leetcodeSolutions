fb19a4caa84b519ffff603c04fcc53d06a6cbd9b
aviral pandey <aviralpandey8604@gmail.com>
2026-08-19 14:28:17 +0530

    Time: 4 ms (45.16%) | Memory: 45.2 MB (35.97%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+        StringBuilder ns=new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch>='a' && ch<='z'  || ch>='0' && ch<='9'){
+                ns.append(ch);
+            }
+        }
+        
+        return checkPalin(ns.toString());
+    }
+    boolean checkPalin(String s){
+        String rev= new StringBuilder(s).reverse().toString();
+        return s.equals(rev);
+    }
+}
