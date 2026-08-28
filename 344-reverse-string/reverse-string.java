b44c8eed8a743507b2ac70530879840f981f153d
aviral pandey <aviralpandey8604@gmail.com>
2026-08-28 11:35:23 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (10.81%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -4,8 +4,7 @@ class Solution {
        int j=s.length-1;
        while(i<j){
         char temp=s[i];
-        s[i]=s[j];
-        s[j]=temp;
+        s[i]=s[j];       s[j]=temp;
         i++;j--;
        }
     }
