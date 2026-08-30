39c5ba84e1a52ece73fedfe1c21c5c77954f38a2
aviral pandey <aviralpandey8604@gmail.com>
2026-08-30 13:30:57 +0530

    Time: 2 ms (66.01%) | Memory: 43.4 MB (8.35%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,11 @@
+class Solution {    
+    public char findTheDifference(String s, String t) {   
+             char c = 0;      
+               for (int i = 0; i < s.length(); i++) {      
+         c ^= s.charAt(i);        
+        c ^= t.charAt(i);      
+                        }    
+     c ^= t.charAt(t.length() - 1);  
+        return c;   
+       } 
+        }
