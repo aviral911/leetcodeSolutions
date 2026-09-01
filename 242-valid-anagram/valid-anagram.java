239081345901297967e9e27281a1158aa64d4aa5
aviral pandey <aviralpandey8604@gmail.com>
2026-09-01 10:39:53 +0530

    Time: 4 ms (83.52%) | Memory: 46.3 MB (42.34%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,18 +2,8 @@ class Solution {
 public boolean isAnagram(String s, String t) {
 char a[]=s.toCharArray();
 char b[]=t.toCharArray();
-if(s.length()!=t.length()) return false;
-for(int i=0;i<a.length;i++){
-    boolean flag=false;
-    for(int j=0;j<b.length;j++){
-        if(a[i]==b[j]){
-            b[j]= ' ';
-               flag =true;
-            break;
-         }
-      }
-      if(flag == false) return false;
-   }
-   return true;
- }
+ Arrays.sort(a);
+  Arrays.sort(b);
+   return Arrays.equals(a,b);
+}
 }
