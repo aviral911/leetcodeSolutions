f22ddf402122519ff58d082f5464c822a0840fe4
aviral pandey <aviralpandey8604@gmail.com>
2026-09-01 10:55:08 +0530

    Time: 2 ms (99.65%) | Memory: 44.7 MB (56.39%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,10 +1,13 @@
 class Solution {
-public boolean isAnagram(String s, String t) {
-char a[]=s.toCharArray();
-char b[]=t.toCharArray();
- Arrays.sort(a);
-  Arrays.sort(b);
-   return Arrays.equals(a,b);
-   
-}
+    public boolean isAnagram(String s, String t) {
+        int f1[]=new int[26];
+        for(char ch:s.toCharArray()){
+            f1[ch - 'a']++;
+        }
+        int f2[]=new int[26];
+        for(char ch:t.toCharArray()){
+            f2[ch - 'a']++;
+        }
+        return Arrays.equals(f1,f2);
+    }
 }
