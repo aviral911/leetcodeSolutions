ececc3c5638700bf9bbab926ed826a9fbc3d8095
aviral pandey <aviralpandey8604@gmail.com>
2026-09-21 01:05:30 +0530

    Time: 13 ms (72.83%) | Memory: 46.4 MB (65.49%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,31 +1,24 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int count = 0;
+       int count=0;
 
-        for (int i = 0; i < k; i++) {
-            if (isVowel(s.charAt(i))) {
-                count++;
-            }
+       for(int i=0;i<k;i++){
+        if(isVowel(s.charAt(i))){
+            count ++;
         }
-
-        int max = count;
-
-        for (int i = k; i < s.length(); i++) {
-            if (isVowel(s.charAt(i))) {
-                count++;
-            }
-
-            if (isVowel(s.charAt(i - k))) {
-                count--;
-            }
-
-            max = Math.max(max, count);
+       }
+       int max = count;
+       for(int i=k;i<s.length();i++){
+         if(isVowel(s.charAt(i))){
+            count ++;
         }
-
-        return max;
-    }
-
-    public boolean isVowel(char c) {
+        if(isVowel(s.charAt(i-k))){
+            count --;
+        }
+        max=Math.max(count , max);
+       }
+return max;}
+public boolean isVowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' ||
                c == 'o' || c == 'u';
     }
